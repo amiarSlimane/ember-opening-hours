b957eef07a087f25a20e5b30c749e7f7d37c9c43
amiar <amiarslimane@yandex.ru>
2022-06-26 16:02:24 +0100

    update

--- addon/components/opening-hours.js
+++ addon/components/opening-hours.js
@@ -5,6 +5,12 @@ import { action } from '@ember/object';
 export default class OpeningHoursComponent extends Component {
 
 
+  constructor(...args){
+    super(...args);
+
+    this.openingHours = this.args.openingHours;
+    console.log(' this.openingHours ', this.openingHours);
+  }
   @tracked
   openingHours = []
 
@@ -12,6 +18,9 @@ export default class OpeningHoursComponent extends Component {
   days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
   selectedDay = '';
+
+  currentHourFrom;
+  currentHourTo;
 
   @action
   setSelectedDay(evt) {
@@ -62,7 +71,7 @@ export default class OpeningHoursComponent extends Component {
   removeHour(evt) {
     evt.preventDefault();
     console.log('remove ', evt.target.value);
-
+    let self = this;
     let tab = [];
     this.openingHours.forEach(function (day) {
       let d = {};
@@ -79,15 +88,15 @@ export default class OpeningHoursComponent extends Component {
   addHour(evt) {
     evt.preventDefault();
     console.log('add ', evt.target.value);
-
+    let self = this;
     let tab = [];
-    this.openingHours.forEach(function (day) {
+    this.openingHours.forEach(function (day, index) {
       let d = {}
       d.day = day.day;
       d.hours = day.hours;
       if (day.day == evt.target.value) {
-        let hours = { id: this.hourId, from: Date.now(), to: Date.now() };
-        this.hourId = this.hourId + 1;
+        let hours = { id: self.hourId, from: Date.now(), to: Date.now() };
+        self.hourId = index*1000 + self.hourId + 1;
         d.hours.push(hours)
       }
       tab.push(d);
@@ -98,7 +107,15 @@ export default class OpeningHoursComponent extends Component {
 
   }
 
+  @action
+  setFrom(v) {
+    console.log('setFrom ', v);
+  }
 
+  @action
+  setTo(v) {
+    console.log('setFrom ', v);
+  }
 
   @action
   save() {
